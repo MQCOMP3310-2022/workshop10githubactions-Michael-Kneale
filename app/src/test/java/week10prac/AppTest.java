fb6a7359344c089161ab63ce1b545e9f72e4db1b
Michael-Kneale <michael.kneale@students.mq.edu.au>
2022-05-09 11:38:15 +1000

    Updated one of the unit tests

--- app/src/test/java/week10prac/AppTest.java
+++ app/src/test/java/week10prac/AppTest.java
@@ -185,8 +185,8 @@ public class AppTest {
 
     @Test public void testWeirdEdgeCase() {
         App classUnderTest = new App();
-        for(int i = 3; i<=14;i++){
-            assertFalse("September 1752 is a weird month", classUnderTest.isValidGregorianDate(i,9,2022));
+        for(int i = 3; i<=13;i++){
+            assertFalse("September 1752 is a weird month", classUnderTest.isValidGregorianDate(i,9,1752));
         }
     }  
 }
