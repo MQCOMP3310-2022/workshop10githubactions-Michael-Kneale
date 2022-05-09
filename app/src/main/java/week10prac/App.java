2242f85313c9987da0a6766a97fa5e6f346f45ee
Michael-Kneale <michael.kneale@students.mq.edu.au>
2022-05-09 12:05:30 +1000

    Added checks for February and weird edge case

--- app/src/main/java/week10prac/App.java
+++ app/src/main/java/week10prac/App.java
@@ -39,6 +39,14 @@ public class App {
         if(day == 31 && month == 11) {
             return false;
         }
+
+        if(day != 28 && day != 29 && month == 2) {
+            return false;
+        }
+
+        if(day >= 3 || day <= 13 && month == 9 && year == 1752) {
+            return false;
+        }
         //Some thoughts: 
         //  what about days of the month?
         //  what about leap years?
