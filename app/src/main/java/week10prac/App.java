e564b624b673273bf66a35be6a0a556508ca5c76
Michael-Kneale <michael.kneale@students.mq.edu.au>
2022-05-09 12:57:37 +1000

    Pushed to test branch

--- app/src/main/java/week10prac/App.java
+++ app/src/main/java/week10prac/App.java
@@ -24,7 +24,33 @@ public class App {
         }
 
         //add your own checks here
-        
+        if(day == 31 && month == 4) {
+            return false;
+        }
+
+        if(day == 31 && month == 6) {
+            return false;
+        }
+
+        if(day == 31 && month == 9) {
+            return false;
+        }
+
+        if(day == 31 && month == 11) {
+            return false;
+        }
+
+        if(day != 28 && day != 29 && month == 2) {
+            return false;
+        }
+
+        if(day == 29 && month == 2 && year % 4 == 0) {
+            return false;
+        }
+
+        if(day >= 3 || day <= 13 && month == 9 && year == 1752) {
+            return false;
+        }
         //Some thoughts: 
         //  what about days of the month?
         //  what about leap years?
